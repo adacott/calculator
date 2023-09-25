2851eb8ebfbc2c9bbdea7fe63ef0f6ddf5d12d55
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-25 03:32:38 +0000

    Added negative button support

--- logic.js
+++ logic.js
@@ -1,43 +1,54 @@
-let num1 = 0, num2 = 0, op_toggle = false, operator = "";
+let num1 = "", num2 = "", op_toggle = false, operator = "", result = "";
 const nums = document.querySelectorAll(".input");
 const operators = document.querySelectorAll(".operation");
 const calculate = document.querySelector(".equals");
 const screen = document.querySelector(".screen");
+const back = document.querySelector(".back");
+// const clear
+// const decimal
 console.log(screen.innerHTML);
 
-function displayKey() {
-    screen.innerHTML = `${this.innerHTML}`;
+function round(num) {
+    num = Math.round(num * 100) / 100;
+    return num;
+}
+
+function displayNum() {
     if (!op_toggle) {
-        num1 = this.innerHTML;
+        num1 += `${this.innerHTML}`;
+        if (this.innerHTML == "+/-") {
+            num1 = parseInt(num1) * -1;
+        }
+        if (this.innerHTML == "&lt;=") {
+            console.log("You pressed back")
+        }
+        screen.innerHTML = `${num1}`;
     }
     else if (op_toggle) {
-        screen.innerHTML = `${this.innerHTML}`;
-        num2 = this.innerHTML;
-        console.log(num2);
-        console.log(num1);
+        num2 += `${this.innerHTML}`;
+        if (this.innerHTML == "+/-") {
+            num2 = parseInt(num2) * -1;
+        }
+        screen.innerHTML = `${num2}`;
     }
 }
 
 function selectOperator() {
     operators.forEach(op => op.classList.remove("pressed"));
-    if (this.innerHTML.includes("+")) {
+    if (this.innerHTML.includes("+") && num1 != "") {
         operator = "add";
-        // console.log(`${operator}`);
         this.classList.add("pressed");
     }
-    else if (this.innerHTML.includes("-")) {
+    else if (this.innerHTML.includes("-") && num1 != "") {
         operator = "subtract";
-        // console.log(`${operator}`);
         this.classList.add("pressed");
     }
-    else if (this.innerHTML.includes("*")) {
+    else if (this.innerHTML.includes("*") && num1 != "") {
         operator = "multiply";
-        // console.log(`${operator}`);
         this.classList.add("pressed");
     }
-    else if (this.innerHTML.includes("/")) {
+    else if (this.innerHTML.includes("/") && num1 != "") {
         operator = "divide";
-        // console.log(`${operator}`);
         this.classList.add("pressed");
     }
 
@@ -49,36 +60,39 @@ function selectOperator() {
 function calculateResult() {
     switch (operator) {
         case "add":
-            op_toggle = true;
+            op_toggle = false;
             result = parseInt(num1) + parseInt(num2);
             screen.innerHTML = result;
-            console.log(num1);
-            console.log(num2);
             break;
         case "subtract":
-            op_toggle = true;
-            result = parseInt(num1) + parseInt(num2);
+            op_toggle = false;
+            result = parseInt(num1) - parseInt(num2);
             screen.innerHTML = result;
             break;
         case "multiply":
-            op_toggle = true;
-            result = parseInt(num1) + parseInt(num2);
+            op_toggle = false;
+            result = parseInt(num1) * parseInt(num2);
+            result = round(result);
             screen.innerHTML = result;
             break;
         case "divide":
-            op_toggle = true;
-            result = parseInt(num1) + parseInt(num2);
+            op_toggle = false;
+            if (num1 == 0) {
+                screen.innerHTML = "Rlly?";
+                break;
+            }
+            result = parseInt(num1) / parseInt(num2);
+            result = round(result);
             screen.innerHTML = result;
             break;
     }
+
     operators.forEach(op => op.classList.remove("pressed"));
-    num1 = 0, num2 = 0;
+    num1 = "", num2 = "", operator = "";
 }
 
-nums.forEach(numb => numb.addEventListener("click", displayKey));
+
+nums.forEach(numb => numb.addEventListener("click", displayNum));
 operators.forEach(op => op.addEventListener("click", selectOperator));
 calculate.addEventListener("click", calculateResult);
-// The code below will print the value of the key when pressed.
-// console.log(this.innerHTML);
-// console.log(this.innertext);
 
