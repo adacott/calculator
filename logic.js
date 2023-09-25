c2f4cee91226d52e77f4422b19625c1f0d75693c
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-25 02:12:48 +0000

    Added skeleton function for operations

--- logic.js
+++ logic.js
@@ -1,18 +1,35 @@
-let num1, num2, operator;
-const keys = document.querySelectorAll("button");
+let num1 = 0, num2 = 0, op_toggle = false, operator = "";
+const nums = document.querySelectorAll(".input");
+const operators = document.querySelectorAll(".operation");
 const screen = document.querySelector(".screen");
 console.log(screen.innerHTML);
 
 function displayKey() {
-    console.log(this.innerHTML)
     screen.innerHTML = `${this.innerHTML}`;
+    num1 = this.innerHTML;
+    console.log(num1);
+    // if (op_toggle) {
+    //     screen.innerHTML = `${this.innerHTML}`;
+    //     num2 = this.innerHTML;
+    //     console.log(num2);
+    // }
 }
 
 function operate() {
-
+    switch (operator) {
+        case "add":
+            break;
+        case "subtract":
+            break;
+        case "multiply":
+            break;
+        case "divide":
+            break;
+    }
 }
 
-keys.forEach(key => key.addEventListener("click", displayKey));
+nums.forEach(numb => numb.addEventListener("click", displayKey));
+operators.forEach(op => op.addEventListener("click", operate));
 
 // The code below will print the value of the key when pressed.
 // console.log(this.innerHTML);
